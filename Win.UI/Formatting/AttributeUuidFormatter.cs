ccb7bc9e442e3b581319dc14494861f2e08feaa8
Matt Sollars <ventaur@gmail.com>
2022-06-07 03:08:25 -0400

    Add various formatters

---
+++ Win.UI/Formatting/AttributeUuidFormatter.cs
@@ -0,0 +1,4 @@
+﻿namespace Win.UI.Formatting;
+internal class AttributeUuidFormatter : IUuidFormatter {
+    public string Format(Guid value) => string.Format("[Guid(\"{0:D}\")]", value);
+}
---
+++ Win.UI/Formatting/StrippedUuidFormatter.cs
@@ -0,0 +1,4 @@
+﻿namespace Win.UI.Formatting;
+internal class StrippedUuidFormatter : IUuidFormatter {
+    public string Format(Guid value) => value.ToString("N");
+}
