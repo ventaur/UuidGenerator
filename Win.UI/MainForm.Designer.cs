2abcb9b070f1e96d5b6f69c631478384b5250323
Matt Sollars <ventaur@gmail.com>
2022-06-08 02:26:30 -0400

    Fill combobox with formats

--- Win.UI/MainForm.Designer.cs
+++ Win.UI/MainForm.Designer.cs
@@ -24,18 +24,34 @@ partial class MainForm {
     ///  the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
+            this.cbFormat = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // cbFormat
+            // 
+            this.cbFormat.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFormat.FormattingEnabled = true;
+            this.cbFormat.Location = new System.Drawing.Point(12, 12);
+            this.cbFormat.Name = "cbFormat";
+            this.cbFormat.Size = new System.Drawing.Size(424, 24);
+            this.cbFormat.TabIndex = 0;
+            this.cbFormat.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.cbFormat_DrawItem);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbFormat);
             this.Name = "MainForm";
             this.Text = "UUID Generator";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
     }
 
     #endregion
+
+    private ComboBox cbFormat;
 }
--- Win.UI/MainForm.cs
+++ Win.UI/MainForm.cs
@@ -1,7 +1,52 @@
+using Win.UI.Formatting;
+
 namespace Win.UI;
 
 public partial class MainForm : Form {
+    int _maxNameWidth;
+
+
     public MainForm() {
         InitializeComponent();
+    }
+
+    private int CalculateMaxNameWidth(List<UuidFormat> allFormats) {
+        int maxNameWidth = 0;
+
+        using var graphics = cbFormat.CreateGraphics();
+        foreach (var format in allFormats) {
+            var size = graphics.MeasureString(format.Name, cbFormat.Font);
+            maxNameWidth = Math.Max(maxNameWidth, (int)size.Width);
+        }
+
+        return maxNameWidth;
+    }
+
+    private void MainForm_Load(object sender, EventArgs e) {
+        var allFormats = UuidFormat.List.OrderBy(f => f.Value).ToList();
+
+        _maxNameWidth = CalculateMaxNameWidth(allFormats);
+
+        cbFormat.DataSource = allFormats;
+        cbFormat.DisplayMember = nameof(UuidFormat.Name);
+        cbFormat.ValueMember = nameof(UuidFormat.Value);
+    }
+    
+    private void cbFormat_DrawItem(object sender, DrawItemEventArgs e) {
+        var uuidFormat = (UuidFormat)cbFormat.Items[e.Index];
+
+        e.DrawBackground();
+
+        var x = e.Bounds.X;
+        var y = e.Bounds.Y;
+        var width = e.Bounds.Width;
+        var height = e.Bounds.Height;
+
+        var nameRect = new Rectangle(x, y, _maxNameWidth, height);
+        var exampleRect = new Rectangle(_maxNameWidth, y, width - _maxNameWidth, height);
+
+        using var brush = new SolidBrush(e.ForeColor);
+        e.Graphics.DrawString(uuidFormat.Name, e.Font ?? Font, brush, nameRect);
+        e.Graphics.DrawString($":  {uuidFormat.Example}", e.Font ?? Font, brush, exampleRect);
     }
 }
